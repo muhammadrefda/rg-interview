49052c35968250f17778cacf2d74f5f4b4e49e80
refda <muhammadrefda@gmail.com>
2021-07-12 01:01:57 +0700

    Add automation role

---
+++ app/Console/Commands/RoleBootstrap.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Permission\Models\Role;
+
+class RoleBootstrap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'laravel_api:bootstrap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create roles';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $roles = ["Admin"];
+
+        $this->line('------------- Setting Up Roles:');
+
+        foreach ($roles as $role) {
+            $role = Role::updateOrCreate(['name' => $role, 'guard_name' => 'web']);
+            $this->info("Created " . $role->name . " Role");
+//            $this->info("Created " . $role->name);
+
+        }
+
+        $this->line('------------- Application Bootstrapping is Complete: \n');
+    }
+}
